564bc238c1f190723b51219b40ead4103f4193ff
Randy Cullins <randycullins2018@gmail.com>
2025-01-06 10:07:01 -0700

    added home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import { products } from "../data/products";
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Products</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
